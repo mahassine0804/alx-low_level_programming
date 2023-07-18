4f54b1c4480f66bd8ebd492d57baec306037320d
mahassine0804 <mahassinemahassine8@gmail.com>
2023-07-18 11:51:02 -0700

    last commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -17,7 +17,8 @@ void print_times_table(int n);
 int print_sign(int n);
 int print_sign(int n);
 void print_times_table(int n);
-int main(void)
+int main(void);
+void print_times_table(int n);
 
 #endif
 
