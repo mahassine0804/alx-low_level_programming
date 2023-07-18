c6ec5d7a2321513cd4b3a07e23e07b5ba7a7e02a
mahassine0804 <mahassinemahassine8@gmail.com>
2023-07-18 11:46:09 -0700

    commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,3 @@
-/*
- *  * File: 101-natural.c
- *   * Auth: vincent kip
- *    */
-
 #include <stdio.h>
 
 /**
@@ -10,19 +5,17 @@
  *   *        that are multiples of 3 or 5.
  *    *
  *     * Return: Always 0.
- *      */
+ */
 int main(void)
 {
-		int i, sum = 0;
+	int i, sum = 0;
 
-			for (i = 0; i < 1024; i++)
-					{
-								if ((i % 3) == 0 || (i % 5) == 0)
-												sum += i;
-									}
-
-				printf("%d\n", sum);
-
-					return (0);
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
 
