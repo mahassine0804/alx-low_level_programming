d00a40193e219d0d25372624c5d2e8887ad08d23
mahassine0804 <mahassinemahassine8@gmail.com>
2023-08-03 10:25:19 -0700

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include"main"
+#include <stdio.h>
+/**
+ *  * main - check the code
+ *   *@s:character to be printed
+ *    * Return: Always 0.
+ */
+void _puts_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+/**
+ * Desc:header file containing functions declaration
+ * *void prototypes(void)
+ * *int prototypes(int)
+ * *char prototypes(char)
+ * @s:character to be printed
+ * Desc:header file containing functions declaration
+*/
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _putchar(char c);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+#endif /* MAIN_H */
