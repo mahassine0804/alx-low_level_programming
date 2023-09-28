3b6330333e49753825a98a6c64f81ab808d5f030
mahassine0804 <mahassinemahassine8@gmail.com>
2023-09-28 16:24:20 -0700

    fix error

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,19 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-/**
- * Desc:header file containing functions declaration
- * *void prototypes(void)
- * *int prototypes(int)
- * *char prototypes(char)
- * @s:character to be printed
- * Desc:header file containing functions declaration
-*/
 void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
 int _putchar(char c);
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
-#endif /* MAIN_H */
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+#endif
