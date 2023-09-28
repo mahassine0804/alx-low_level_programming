7d4392805b98f9a3d0cba7fa23f4413f8265d783
mahassine0804 <mahassinemahassine8@gmail.com>
2023-09-28 16:40:41 -0700

    fix error in exercice num 0

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * * factorial - searchs for the factorial of a number
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
  * * _pow_recursion - function that returns the
  * * value of x raised to the power of y
