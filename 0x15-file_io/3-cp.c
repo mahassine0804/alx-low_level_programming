6208604ea8e1bae041b0a6b412a3c9c794935e59
mahassine0804 <mahassinemahassine8@gmail.com>
2023-11-01 14:50:29 -0700

    modify task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@ void close_file(int fd);
  *    *
  *     * Return: A pointer to the newly-allocated buffer.
  */
-char *creates_buffer(char *file)
+char *create_buffer(char *file)
 {
 	char *buffer;
 
